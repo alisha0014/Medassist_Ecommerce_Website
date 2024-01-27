493d027e5f2680dc3804ad5acff6ae514586e53e
alisha0014 <120816659+alisha0014@users.noreply.github.com>
2024-01-27 14:32:14 +0530

    Add files via upload

---
+++ medassist_ecom/medassist_ecom/CheckSession.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.views.decorators.clickjacking import xframe_options_exempt
+@xframe_options_exempt
+def Security(request,page):
+    try:
+     admin=request.session['ADMIN']
+     return render(request,page)
+    except:
+     return render(request,'Login_Page.html')
